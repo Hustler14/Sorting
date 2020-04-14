da5d6236572ec8458fb0db8d7a7a6aaa6a3f9fbf
Hustler14 <41165664+Hustler14@users.noreply.github.com>
2020-04-14 21:31:38 +0530

    Update Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,12 +1,13 @@
+#sorting in ascending order
 a = [1,6,8,0,3,9,7,4,2,5]
-for i in range(0,len(a)):
-    mini = a[i]
-    pos = i
-    for j in range(i,len(a)):
-        if a[j]<=mini:
-            mini = a[j]
+for i in range(0,len(a)): 
+    mini = a[i]                         #set element in position i as minimum for the iteration. 
+    pos = i                             #position.
+    for j in range(i,len(a)):           #run loop from i to length(a)
+        if a[j]<=mini:                  
+            mini = a[j]                 #if jth element is lesser than minimum, update minimum and position.
             pos = j
-    dummy = a[i]
+    dummy = a[i]                        #swap ith element with minimum element using it position.
     a[i] = mini
     a[pos] = dummy
 print(a)
