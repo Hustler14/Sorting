a40195b277d22899a67450d7dc05845cf5de322a
Hustler14 <41165664+Hustler14@users.noreply.github.com>
2020-04-15 10:02:47 +0530

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -2,7 +2,7 @@
 a = [1,6,8,0,3,9,7,4,2,5]
 for i in range(0,len(a)): 
     for j in range(0,len(a)-i-1):                       #largest number gets bubbled to the right
-        if a[j+1]<=a[j]:                                #checking adjacent numbers and swap them based on requirement
+        if a[j+1]<=a[j]:                                #check adjacent numbers and swap them based on requirement
             dummy = a[j]
             a[j] = a[j+1]
             a[j+1] = dummy
